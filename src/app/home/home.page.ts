6a8100ba31dca8bfb3adb5d39b80dd677bd9439e
Roland Jethro Suyom <rolandjethrosuyom@yahoo.com>
2023-02-04 07:26:41 +0300

    finished home page styling

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,58 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  suppliers = [
+    {
+      id: 1,
+      name: 'gsk',
+      image: 'capsules',
+      code: 74
+    },
+    {
+      id: 2,
+      name: 'aspen',
+      image: 'tablets',
+      code: 85
+    },
+    {
+      id: 3,
+      name: 'roche',
+      image: 'vials',
+      code: 89
+    },
+    {
+      id: 4,
+      name: 'atnahs',
+      image: 'syringe',
+      code: 19
+    },
+    {
+      id: 5,
+      name: 'msd',
+      image: 'heart-pulse',
+      code: '09'
+    },
+    {
+      id: 6,
+      name: 'pharma',
+      image: 'heart',
+      code: 79
+    },
+    {
+      id: 7,
+      name: 'organon',
+      image: 'pills',
+      code: 18
+    }
+  ]
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  goToSupplier(supplier: string) {
+    console.log(supplier);
+    this.router.navigate(['supplier/', { name: supplier }]);
+  }
 
 }
---
+++ src/app/supplier/supplier-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { SupplierPage } from './supplier.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SupplierPage
+  },
+  {
+    path: 'supplier/:id',
+    component: SupplierPage
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class SupplierPageRoutingModule { }
