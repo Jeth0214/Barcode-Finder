1a9bd9851580d654b46d6296d570f6cce2c34b09
Roland Jethro Suyom <rolandjethrosuyom@yahoo.com>
2023-03-03 18:31:12 +0300

    set up login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
     path: 'supplier/:id',
     loadChildren: () => import('./supplier/supplier.module').then(m => m.SupplierPageModule)
-  }
+  },
+
 
 ];
 
