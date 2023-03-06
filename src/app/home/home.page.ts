8c7dbe2ab0981b9d3b822fe15735b2f04d9470db
Roland Jethro Suyom <rolandjethrosuyom@yahoo.com>
2023-03-06 22:22:29 +0300

    implemented log out feature

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,8 @@ import { Supplier } from '../models/supplier.model';
 import { SuppliersService } from '../services/suppliers.service';
 import { LoadingController, AlertController } from '@ionic/angular';
 import { HttpErrorResponse } from '@angular/common/http';
+import { AuthenticationService } from '../services/authentication.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -14,19 +16,24 @@ export class HomePage implements OnInit {
 
   suppliers: Supplier[] = [];
   hasSuppliers: boolean = false;
+  user: User;
+  isLoading: boolean = false;
 
   constructor(
     private router: Router,
     private suppliersService: SuppliersService,
     private loadingController: LoadingController,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private authService: AuthenticationService
   ) { }
 
   ngOnInit() {
     this.getSuppliers();
+    this.user = this.authService.currentUserValue;
   }
 
   async getSuppliers() {
+    this.isLoading = true;
     const loading = await this.loadingController.create({
       message: 'Loading suppliers..',
       spinner: 'circles'
@@ -36,10 +43,12 @@ export class HomePage implements OnInit {
     this.suppliersService.getAllSuppliers().subscribe(
       async (response: Supplier[]) => {
         await loading.dismiss();
+        this.isLoading = false;
         this.suppliers = response;
         this.hasSuppliers = response.length <= 0;
       },
       async (error: HttpErrorResponse) => {
+        this.isLoading = false;
         await loading.dismiss();
         this.alertError('Error', error.status, error.statusText);
       }
@@ -70,4 +79,9 @@ export class HomePage implements OnInit {
     await alert.present();
   }
 
+  onLogOut() {
+    console.log('log out');
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
