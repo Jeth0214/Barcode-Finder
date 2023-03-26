5660e15b2b06d801b4329c5370e0ad41ea3f0376
Roland Jethro Suyom <rolandjethrosuyom@yahoo.com>
2023-03-26 08:12:22 +0300

    created apk for testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiBaseUrl: 'http://127.0.0.1:8000/api',
-  // apiBaseUrl: 'http://192.168.100.17:8000/api',
+  // apiBaseUrl: 'http://com.Jeth.barcodeFinder:8000/api',
   apiBaseUrl: 'http://192.168.250.38:8000/api',
 };
 
